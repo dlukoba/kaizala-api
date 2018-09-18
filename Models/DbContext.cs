c911605bebdc0b069fed037071b6fd2faf0eb96d
dlukoba_yahoo_com <dlukoba@yahoo.com>
2018-09-18 22:24:56 +0300

    added dbcontext modelbuilder method

--- Models/DbContext.cs
+++ Models/DbContext.cs
@@ -4,10 +4,24 @@ namespace saf_kaizala_api.Models
 {
     public class DatabaseContext : DbContext
     {
+        public DbSet<Customer> Customers { get; set; }
+
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : 
             base (options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .ToTable("customers")
+                .HasKey(c => c.Id);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
         }
     }
 }
